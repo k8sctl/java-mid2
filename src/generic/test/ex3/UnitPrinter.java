0641b2332f5b998e6825d61de77089c595012b8c
thbins <joons0218@gmail.com>
2026-04-03 16:42:55 +0900

    학습: 와일드 카드와 타입 이레이저

---
+++ src/generic/test/ex3/UnitPrinter.java
@@ -0,0 +1,20 @@
+package generic.test.ex3;
+
+import generic.test.ex3.unit.BioUnit;
+
+public class UnitPrinter {
+
+    public static void printV0(Shuttle shuttle){
+        shuttle.showInfo();
+    }
+
+    public static <T extends BioUnit> void printV1(Shuttle<T> shuttle) {
+        T unit = shuttle.out();
+        System.out.println("이름: " + unit.getName() + ", HP: " + unit.getHp());
+    }
+
+    public static void printV2(Shuttle<? extends BioUnit> shuttle) {
+        BioUnit unit = shuttle.out();
+        System.out.println("이름: " + unit.getName() + ", HP: " + unit.getHp());
+    }
+}
---
+++ src/generic/test/ex3/UnitUtil.java
@@ -0,0 +1,10 @@
+package generic.test.ex3;
+
+import generic.test.ex3.unit.BioUnit;
+
+public class UnitUtil {
+
+    public static <T extends BioUnit> T maxHp(T unit1, T unit2) {
+        return unit1.getHp() > unit2.getHp() ? unit1 : unit2;
+    }
+}
