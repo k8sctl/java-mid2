451ea7de089eb4111b27e40b57d517a544f2d5cc
thbins <joons0218@gmail.com>
2026-04-03 21:25:07 +0900

    학습: ArrayList 구현 및 원리 학습

---
+++ src/collection/array/MyArrayListV2.java
@@ -0,0 +1,72 @@
+package collection.array;
+
+import java.util.Arrays;
+
+public class MyArrayListV2 {
+
+    private static final int DEFAULT_CAPACITY = 5;
+
+    private Object[] elementData;
+    private int size = 0;
+
+    public MyArrayListV2() {
+        elementData = new Object[DEFAULT_CAPACITY];
+    }
+
+    public MyArrayListV2(int initialCapacity) {
+        elementData = new Object[initialCapacity];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void add(Object e) {
+        // 코드 추가
+        if (size == elementData.length) {
+            grow();
+        }
+        elementData[size++] = e;
+    }
+
+    // 코드 추가
+    public void grow() {
+        int oldCapacity = elementData.length;
+        int newCapacity = oldCapacity * 2;
+        // 배열을 새로 만들고, 기존 배열을 새로운 배열에 복사한다.
+        /*
+        Object[] newArr = new Object[newCapacity];
+        for (int i = 0 ; i < elementData.length ; i++) {
+            newArr[i] = elementData[i];
+        }
+        */
+        Object[] newArr = Arrays.copyOf(elementData, newCapacity);
+        // 새로운 배열을 참조하도록 변경한다.
+        elementData = newArr;
+    }
+
+    public Object get(int index) {
+        return elementData[index];
+    }
+
+    public Object set(int index, Object element) {
+        Object oldValue = elementData[index];
+        elementData[index] = element;
+        return oldValue;
+    }
+
+    public int indexOf(Object o) {
+        for (int i = 0 ; i < size ; i++) {
+            if (o.equals(elementData[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(Arrays.copyOf(elementData, size)) +
+                ", size = " + size + ", capacity = " + elementData.length; // .copyOf()는 elementData에서 size만큼만 복사
+    }
+}
