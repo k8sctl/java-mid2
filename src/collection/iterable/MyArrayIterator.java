46e41e665689e33f3ef6c30a3737df1d2c019d16
thbins <joons0218@gmail.com>
2026-04-10 20:32:58 +0900

    학습: iterable과 iterator 학습

---
+++ src/collection/iterable/MyArrayIterator.java
@@ -0,0 +1,26 @@
+package collection.iterable;
+
+import java.util.Iterator;
+
+public class MyArrayIterator implements Iterator<Integer> {
+
+    private int currentIndex = -1;
+    private int[] targetArr;
+
+    public  MyArrayIterator(int[] targetArr) {
+        this.targetArr = targetArr;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return currentIndex < targetArr.length -1;
+    }
+
+    @Override
+    public Integer next() {
+        // return targetArr[++currentIndex];
+        currentIndex++;
+        int result = targetArr[currentIndex];
+        return result;
+    }
+}
